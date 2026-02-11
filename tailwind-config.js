b06cb06f2acba7bca54a156d68c3bd1b971b39a4
Markklol <markkdumb2@gmail.com>
2026-02-11 20:24:08 +0800

    Create tailwind-config.js

---
+++ tailwind-config.js
@@ -0,0 +1,21 @@
+const colors = {
+  primary: '#ffdd95',
+  background: '#242428',
+  lightbg: '#2f2f33',
+  card: '#2f2f33',
+  btnbg: '#3a3a3e',
+  lighttext: '#ccc',
+}
+export default {
+  theme: {
+    extend: {
+      colors: colors,
+    },
+    fontFamily: {
+      sans: ['poppins', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: 500,
+    },
+  },
+}
