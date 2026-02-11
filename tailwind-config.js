82e7645001379cb5497f74b117025649df42cae4
Markklol <markkdumb2@gmail.com>
2026-02-11 20:28:14 +0800

    Update color values in tailwind-config.js

--- tailwind-config.js
+++ tailwind-config.js
@@ -1,7 +1,7 @@
 const colors = {
-  primary: '#ffdd95',
-  background: '#242428',
-  lightbg: '#2f2f33',
+  primary: '#F5E6C8',
+  background: '#141414',
+  lightbg: '#1c1c1c',
   card: '#2f2f33',
   btnbg: '#3a3a3e',
   lighttext: '#ccc',
