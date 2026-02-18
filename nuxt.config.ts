fefd6fb5bc3648a37267f84e14b978884934135a
eieiddio <jaquelenecanites760@gmail.com>
2026-02-18 17:41:57 +0800

    Add initial Nuxt configuration with modules and settings

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'nuxt-swiper',
+    '@vueuse/nuxt',
+  ],
+  devtools: { enabled: false },
+  css: ['~/assets/css/variables.css'],
+  runtimeConfig: {
+    serverUrl: 'http://localhost:3030/api/v1',
+  },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {},
+  },
+  icon: {
+    mode: 'css',
+    cssLayer: 'base',
+  },
+  image: {
+    format: ['webp'],
+    domains: ['https://cdn.noitatnemucod.net', 'https://cdn.myanimelist.net'],
+  },
+});
